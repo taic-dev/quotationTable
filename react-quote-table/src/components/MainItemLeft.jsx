4fee93fb2903423a54b9757c772e668a1ca072ef
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-14 15:53:54 +0900

    #1 molecules/MainField作成

--- react-quote-table/src/components/MainItemLeft.jsx
+++ react-quote-table/src/components/MainItemLeft.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import TextField from "./atoms/TextFileld";
+import MainField from "./molecules/MainField";
 
 const MainItemLeft = () => {
   return (
-      <div className="main-item main-item__left">
-        <TextField labelName="会社名" text="御中" />
-        <TextField labelName="ご担当者" text="様" />
-        <TextField labelName="件名" option="fullWidth" />
-        <p>下記の通り、お見積り申し上げます。</p>
-        <TextField labelName="納期" />
-        <TextField labelName="支払い条件" />
-        <TextField labelName="有効期限" />
-    </div>
+    <MainField>
+      <TextField labelName="会社名" text="御中" />
+      <TextField labelName="ご担当者" text="様" />
+      <TextField labelName="件名" option="fullWidth" />
+      <p>下記の通り、お見積り申し上げます。</p>
+      <TextField labelName="納期" />
+      <TextField labelName="支払い条件" />
+      <TextField labelName="有効期限" />
+    </MainField>
   );
 };
 
--- react-quote-table/src/components/MainItemRight.jsx
+++ react-quote-table/src/components/MainItemRight.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 import TextField from "./atoms/TextFileld";
+import MainField from "./molecules/MainField";
 
 const MainItemRight = () => {
   return (
-    <div className="main-item main-item__right">
-        <TextField labelName="見積もりNo." />
-        <TextField labelName="見積もり日" />
-        <TextField labelName="自社名" />
-        <TextField labelName="〒" />
-        <TextField labelName="住所" />
-        <TextField labelName="電話番号" />
-        <TextField labelName="FAX" />
-        <TextField labelName="E-mail" />
-        <TextField labelName="担当" />
-    </div>
-  )
-}
+    <MainField>
+      <TextField labelName="見積もりNo." />
+      <TextField labelName="見積もり日" />
+      <TextField labelName="自社名" />
+      <TextField labelName="〒" />
+      <TextField labelName="住所" />
+      <TextField labelName="電話番号" />
+      <TextField labelName="FAX" />
+      <TextField labelName="E-mail" />
+      <TextField labelName="担当" />
+    </MainField>
+  );
+};
 
-export default MainItemRight
+export default MainItemRight;
