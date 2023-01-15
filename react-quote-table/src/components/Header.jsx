1a3eed145982a0bcd236d222e847adde6ae25fd2
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-15 18:06:34 +0900

    #1 templates/Main作成

--- react-quote-table/src/components/Header.jsx
+++ react-quote-table/src/components/Header.jsx
@@ -1,39 +1,49 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Toolbar, Typography, Button, Box, AppBar } from '@mui/material'
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import Main from './organisms/Main'
-import Pdf from './Pdf'
-import React, { useState } from 'react'
+import { Toolbar, Typography, Button, Box, AppBar } from "@mui/material";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import Main from "./templates/Main";
+import Pdf from "./Pdf";
+import React, { useState } from "react";
 
 const Header = () => {
-    const [allInfo,setAllInfo] = useState([]);
-    const [totalPrice,setTotalPrice] = useState(0);
+  const [allInfo, setAllInfo] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   return (
     <BrowserRouter>
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Link to="/" style={{color: "#fff"}}>お見積書 作成システム</Link>
-                </Typography>
-                <Link to="/pdf">
-                    <PictureAsPdfIcon />
-                </Link>
-                </Toolbar>
-            </AppBar>
-        </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/" style={{ color: "#fff" }}>
+                お見積書 作成システム
+              </Link>
+            </Typography>
+            <Link to="/pdf">
+              <PictureAsPdfIcon />
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
-        <Switch>
-            <Route exact path="/">
-                <Main setAllInfo={setAllInfo} totalPrice={totalPrice} setTotalPrice={setTotalPrice}></Main>
-            </Route>
-            <Route exact path="/pdf" style={{ color: "red", textDecoration: "none" }}>
-                <Pdf allInfo={allInfo} totalPrice={totalPrice}></Pdf>
-            </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            setAllInfo={setAllInfo}
+            totalPrice={totalPrice}
+            setTotalPrice={setTotalPrice}
+          ></Main>
+        </Route>
+        <Route
+          exact
+          path="/pdf"
+          style={{ color: "red", textDecoration: "none" }}
+        >
+          <Pdf allInfo={allInfo} totalPrice={totalPrice}></Pdf>
+        </Route>
+      </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
