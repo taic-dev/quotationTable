2adb6b42d88915724d1c2d4bcbd9da59875a7f70
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-25 00:59:31 +0900

    #17 onChangeで2つのイベントを発火するようにした

--- react-quote-table/src/components/atoms/TextFileld/index.jsx
+++ react-quote-table/src/components/atoms/TextFileld/index.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import { TextField } from "@mui/material";
 import styles from "./index.module.css";
 
-const index = ({ options }) => {
+const Index = ({ options, detailInfo, setDetailInfo }) => {
+  const handleChangeTest = () => {
+    console.log("test");
+  }
 
+  const changeDetailInfo = (e) =>{
+    const { name, value } = e.target;
+    setDetailInfo({...detailInfo, [name]: value});
+  }
+  
   return (
-    <div className={styles['main-textarea']}>
-      <TextField 
+    <div className={styles["main-textarea"]}>
+      <TextField
         id="outlined-required"
         variant="standard"
-        label={ options[0].labelName && options[0].labelName }
-        value={ options[0].value && options[0].value }
-        defaultValue={ options[0].defaultValue && options[0].defaultValue }
-        type={ options[0].type && options[0].type }
-        name={ options[0].name && options[0].name }
-        fullWidth={ options[0].fullWidth && true }
-        onChange={ options[0].onChange && options[0].onChange }
-        />
-      { options[0].text && <span>{ options[0].text }</span>}
+        label={options[0].labelName && options[0].labelName}
+        value={options[0].value && options[0].value}
+        defaultValue={options[0].defaultValue && options[0].defaultValue}
+        type={options[0].type && options[0].type}
+        name={options[0].name && options[0].name}
+        fullWidth={options[0].fullWidth && true}
+        onChange={(e)=>{
+          handleChangeTest();
+          options[0].onChange && changeDetailInfo(e);
+        }}
+      />
+      {options[0].text && <span>{options[0].text}</span>}
     </div>
   );
 };
 
-export default index;
+export default Index;
--- react-quote-table/src/components/molecules/MainLeft/index.jsx
+++ react-quote-table/src/components/molecules/MainLeft/index.jsx
@@ -4,19 +4,68 @@ import TextField from "../../atoms/TextFileld";
 import StampImg from "../../atoms/StampImg";
 
 const index = ({ detailInfo, setDetailInfo }) => {
-
-  const changeDetailInfo = (e) =>{
-    const { name, value } = e.target;
-    setDetailInfo({...detailInfo, [name]: value});
-  }
-
   return (
     <MainField>
-      <TextField options={[{ labelName: "会社名", text: "御中", name: "company", onChange: (e)=>changeDetailInfo(e) }]} />
-      <TextField options={[{ labelName: "〒", fullWidth: true, name: "postalCode", onChange: (e)=>changeDetailInfo(e) }]} />
-      <TextField options={[{ labelName: "住所", fullWidth: true, name: "address", onChange: (e)=>changeDetailInfo(e) }]} />
-      <TextField options={[{ labelName: "ご担当者", text: "様", name: "rep", onChange: (e)=>changeDetailInfo(e) }]} />
-      <TextField options={[{ labelName: "件名", fullWidth: true, name: "subject", onChange: (e)=>changeDetailInfo(e) }]} />
+      <TextField
+        detailInfo={detailInfo}
+        setDetailInfo={setDetailInfo}
+        options={[
+          {
+            labelName: "会社名",
+            text: "御中",
+            name: "company",
+            onChange: true,
+          },
+        ]}
+      />
+      <TextField
+        detailInfo={detailInfo}
+        setDetailInfo={setDetailInfo}
+        options={[
+          {
+            labelName: "〒",
+            fullWidth: true,
+            name: "postalCode",
+            onChange: true,
+          },
+        ]}
+      />
+      <TextField
+        detailInfo={detailInfo}
+        setDetailInfo={setDetailInfo}
+        options={[
+          {
+            labelName: "住所",
+            fullWidth: true,
+            name: "address",
+            onChange: true,
+          },
+        ]}
+      />
+      <TextField
+        detailInfo={detailInfo}
+        setDetailInfo={setDetailInfo}
+        options={[
+          { 
+            labelName: "ご担当者", 
+            text: "様", 
+            name: "rep", 
+            onChange: true 
+          },
+        ]}
+      />
+      <TextField
+        detailInfo={detailInfo}
+        setDetailInfo={setDetailInfo}
+        options={[
+          {
+            labelName: "件名",
+            fullWidth: true,
+            name: "subject",
+            onChange: true,
+          },
+        ]}
+      />
       <StampImg detailInfo={detailInfo} setDetailInfo={setDetailInfo} />
     </MainField>
   );
